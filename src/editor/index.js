ed6cdca37a20573c34cad61381b2a47f2752fccd
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 22:07:40 +0100

    FW-007 hash normalized canonical editor definitions

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,9 +1,35 @@
-export {
+import { canonicalHash } from '../core/canonical.js';
+import {
   EDITOR_VERSION,
   DEFAULT_EDITOR_SEED,
   EDITOR_TOOLS,
   EDITOR_SPEEDS,
   worldPositionFromUnits,
   worldPositionToUnits,
-  EditorSession
+  EditorSession as BaseEditorSession
 } from './model.js';
+
+export {
+  EDITOR_VERSION,
+  DEFAULT_EDITOR_SEED,
+  EDITOR_TOOLS,
+  EDITOR_SPEEDS,
+  worldPositionFromUnits,
+  worldPositionToUnits
+};
+
+export class EditorSession extends BaseEditorSession {
+  authoringHash() {
+    if (this._cachedAuthoringRevision === this._authoringRevision && this._cachedAuthoringHash) return this._cachedAuthoringHash;
+    const hash = canonicalHash({
+      version: EDITOR_VERSION,
+      rootSeed: this.rootSeed,
+      fields: this.fieldCollection.toCanonical(),
+      materials: this.simulation.materials,
+      emitters: this.simulation.emitters
+    });
+    this._cachedAuthoringRevision = this._authoringRevision;
+    this._cachedAuthoringHash = hash;
+    return hash;
+  }
+}
