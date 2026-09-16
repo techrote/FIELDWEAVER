507bd5a7dec2213fe19ffaa813d8c156fc3a77fd
tom <261801702+techrote@users.noreply.github.com>
2026-09-17 00:09:59 +0100

    FW-011 export mutation API

---
+++ src/mutation/index.js
@@ -0,0 +1,12 @@
+export {
+  MUTATION_VERSION,
+  LINEAGE_VERSION,
+  MUTATION_SCOPES,
+  MUTATION_INTENSITIES,
+  MUTATION_OPERATOR_TYPES,
+  MutationRejectedError,
+  normalizeMutationConfig,
+  diffRecipes,
+  mutateRecipe,
+  createSiblingVariants
+} from './model.js';
