fdd1d86279e131dd18db13729da385b4c9012c8d
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 23:32:18 +0100

    FW-010 export timeline model

---
+++ src/timeline/index.js
@@ -0,0 +1,10 @@
+export {
+  TIMELINE_VERSION,
+  REPLAY_CHECKPOINT_VERSION,
+  DEFAULT_CHECKPOINT_INTERVAL,
+  DEFAULT_MAX_CHECKPOINTS,
+  earliestChangedCommandTick,
+  createReplayCheckpoint,
+  restoreReplayCheckpoint,
+  TimelineReplayController
+} from './model.js';
