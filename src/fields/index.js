760d982b799db41a10a8b104b187a7cfa49f885c
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 19:14:35 +0100

    FW-003: deterministic chunked field authoring substrate

---
+++ src/fields/index.js
@@ -0,0 +1,31 @@
+export {
+  FIELD_FORMAT_VERSION,
+  FIELD_KINDS,
+  FIELD_BLEND_MODES,
+  DEFAULT_CELLS_PER_CHUNK,
+  MIN_CELLS_PER_CHUNK,
+  MAX_CELLS_PER_CHUNK,
+  normalizeFieldCellAddress,
+  fieldCellToGlobalGrid,
+  globalGridToFieldCell,
+  worldPositionToFieldCell,
+  SparseFieldLayer,
+  FieldLayerCollection
+} from './model.js';
+export {
+  BRUSH_OPERATIONS,
+  BRUSH_SHAPES,
+  BRUSH_FALLOFFS,
+  MAX_BRUSH_RADIUS,
+  MAX_STROKE_GRID_STEPS,
+  normalizeBrush,
+  canonicalizeStroke,
+  createBrushPatch,
+  applyBrushPatch
+} from './brush.js';
+export {
+  AuthoringHistory,
+  createBrushCommand,
+  createLayerEnabledCommand,
+  createLayerMoveCommand
+} from './history.js';
